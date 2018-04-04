549f089f4d88d218d2b2e6c67e7df0dcca48109a
Jean-Marie Porchet <jm@jmporchet.ch>
2018-04-04 12:26:58 +0200

    initial commit, base features

---
+++ App/Utils/api.js
@@ -0,0 +1,26 @@
+export const api = {
+  getBio (username) {
+    username = username.toLowerCase().trim();
+    const url = `https://api.github.com/users/${username}`;
+    return fetch(url).then((res) => res.json())
+  },
+  getRepos (username) {
+    username = username.toLowerCase().trim();
+    const url = `https://api.github.com/users/${username}/repos`;  
+    return fetch(url).then((res) => res.json())
+  },
+  getNotes (username) {
+    username = username.toLowerCase().trim();
+    const url = `https://githubnotetaker-27067.firebaseio.com/${username}.json`;  
+    return fetch(url).then((res) => res.json())
+  },
+  postNote (username, note) {
+    username = username.toLowerCase().trim();
+    const url = `https://githubnotetaker-27067.firebaseio.com/${username}.json`;  
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json())
+    .catch(err => console.log(err));
+  }
+}
